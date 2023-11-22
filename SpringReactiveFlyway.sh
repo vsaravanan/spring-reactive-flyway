b593dc79662da169f280a320efb42ec19424d173
saravanan <saravanan.resume@gmail.com>
2023-11-22 14:18:41 +0000

    .

--- SpringReactiveFlyway.sh
+++ SpringReactiveFlyway.sh
@@ -3,6 +3,6 @@ jarname=SpringReactiveFlyway
 rm -rf $rootdir/logs/*
 rm -rf $rootdir/logPath_IS_UNDEFINED
 rm -rf /data/logs/spring-reactive
-# java -jar -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=*:5566,suspend=n -Dspring.profiles.active=dev  ./$jarname.jar
+## java -jar -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=*:5566,suspend=n -Dspring.profiles.active=dev  ./$jarname.jar
 java -jar target/$jarname.jar
 
