ff9ca4cbe4053d2c144b7179e518cf50c171defa
saravanan <saravanan.resume@gmail.com>
2023-11-23 15:05:52 +0000

    SpringReactiveFlyway implemented

--- SpringReactiveFlyway.sh
+++ SpringReactiveFlyway.sh
@@ -3,6 +3,7 @@ jarname=SpringReactiveFlyway
 rm -rf $rootdir/logs/*
 rm -rf $rootdir/logPath_IS_UNDEFINED
 rm -rf /data/logs/spring-reactive
+# mvn clean package install -Dmaven.test.skip=true -T 1C
 ## java -jar -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=*:5566,suspend=n -Dspring.profiles.active=dev  ./$jarname.jar
-java -jar target/$jarname.jar
+java -jar target/$jarname.jar --port=10100
 
