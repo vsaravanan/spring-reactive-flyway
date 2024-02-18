b0ac235c6f93e980f4e7a77336e35fb9e0234da9
viswa <saravanan.resume@gmail.com>
2024-02-18 23:32:47 +0800

    sjsdb

--- src/main/java/com/saravan/reactive/student/StudentController.java
+++ src/main/java/com/saravan/reactive/student/StudentController.java
@@ -16,7 +16,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/api/v1/students")
+@RequestMapping("/api2/v1/students")
 @RequiredArgsConstructor
 public class StudentController {
 
