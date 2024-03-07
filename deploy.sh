bd2f2c52d499078d3616e957026c94ee2468c98b
viswa <saravanan.resume@gmail.com>
2024-03-07 21:53:16 +0800

    Jenkinsfile

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 git pull
 mvn clean package install -Dmaven.test.skip=true -T 1C
-# sudo service spring-reactive-flyway restart
 pm2 restart SpringReactiveFlyway
 tail -f /data/logs/SpringReactiveFlyway/*
